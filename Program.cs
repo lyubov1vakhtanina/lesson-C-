b16a8a3c871270430d3530f134a6afc4c348f13d
Любовь Вахтанина <135403130+lyubov1vakhtanina@users.noreply.github.com>
2023-07-04 17:24:20 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,98 +1,54 @@
-﻿// Console.WriteLine("Введите число");
-
-// int number = Convert.ToInt32 (Console.ReadLine());
-// int num2 = number*number;
-
-
-// 1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
-// a = 25, b = 5 -> да 
-// a = 2, b = 10 -> нет 
-// a = 9, b = -3 -> да 
-// a = -3 b = 9 -> нет
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
 
 // Console.WriteLine("Введите число");
-// int number = Convert.ToInt32 (Console.ReadLine());
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a > b)
+// System.Console.WriteLine("Большее число: " + a + ", меньшее число: " + b);
+// else System.Console.WriteLine($"Большее число: {a}, меньшее число: {b}");
 
+//Задача2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+ 
+
+// Console.WriteLine("Введите первое число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+// if (max < b) max = b;
+// if (max < c) max = c;
+
+// System.Console.WriteLine("Максимальное из этих чисел = »"+ max); 
+
+
+// Задача3. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+ 
 // Console.WriteLine("Введите число");
-// int number2 = Convert.ToInt32 (Console.ReadLine());
+// int a = Convert.ToInt32(Console.ReadLine());
+// if (a % 2 == 0)
+// System.Console.WriteLine("Число: " + a + " четное");
+// else System.Console.WriteLine($"Число: {a} нечетное");
 
-// if (number==number2*number2)
-// {
-//     Console.WriteLine("Да");
-// }
-// else
-// {
-//     Console.WriteLine("Нет");
-// }
-
-// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
-// 	3 -> Среда 
-// 5 -> Пятница
-
-// Console.WriteLine ("Введите число");
-// int number = Convert.ToInt32 (Console.ReadLine());
-
-// if(number==1)
-// {
-//     Console.WriteLine("Понедельник");
-// }
-// else if(number==2)
-// {
-//     Console.WriteLine("Вторник");
-
-// }
-// else if(number==3)
-// {
-//     Console.WriteLine("Среда");
-// }
-// else if(number==4)
-// {
-//     Console.WriteLine("Четверг");
-
-// }
-// else if(number==5)
-// {
-//     Console.WriteLine("Пятница");
-
-// }
-// else if(number==6)
-// {
-//     Console.WriteLine("Суббота");
-// }
-// else if(number==7)
-// {
-//     Console.WriteLine("Воскресенье");
-
-// }
-// else
-// {
-//     Console.WriteLine("Такого дня недели у нас нет");
-// }
-
-// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
-// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
-// 2 -> " -2, -1, 0, 1, 2"
-
-// Console.WriteLine("Введите число");
-// int num = Convert.ToInt32(Console.ReadLine());
-
-// int negNumber= -1*num;
-
-// while(negNumber<=num)
-// {
-//     Console.WriteLine(negNumber);
-//     negNumber=negNumber+1;
-
-// }
-
-// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
-// 	456 -> 6
-// 	782 -> 2
-// 	918 -> 8
-
-
-// Console.WriteLine("Введите число");
-// int num = Convert.ToInt32(Console.ReadLine());
-
-// int result= num%10;
-// Console.WriteLine(result);
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+ 
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+for(int i=1;i<=n;i++)
+{
+if (i % 2 == 0)
+System.Console.Write(i + ", ");
+}

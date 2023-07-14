dd296029b9f60c5cb5352cc368df955503bb8dd7
Любовь Вахтанина <135403130+lyubov1vakhtanina@users.noreply.github.com>
2023-07-14 19:59:02 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,54 +1,71 @@
-﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-// a = 5; b = 7 -> max = 7
-// a = 2 b = 10 -> max = 10
-// a = -9 b = -3 -> max = -3
-
-// Console.WriteLine("Введите число");
-// int a = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите второе число");
-// int b = Convert.ToInt32(Console.ReadLine());
-// if (a > b)
-// System.Console.WriteLine("Большее число: " + a + ", меньшее число: " + b);
-// else System.Console.WriteLine($"Большее число: {a}, меньшее число: {b}");
-
-//Задача2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-// 2, 3, 7 -> 7
-// 44 5 78 -> 78
-// 22 3 9 -> 22
- 
-
-// Console.WriteLine("Введите первое число");
-// int a = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите второе число");
-// int b = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите третье число");
-// int c = Convert.ToInt32(Console.ReadLine());
-// int max = a;
-// if (max < b) max = b;
-// if (max < c) max = c;
-
-// System.Console.WriteLine("Максимальное из этих чисел = »"+ max); 
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
 
 
-// Задача3. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
-// 4 -> да
-// -3 -> нет
-// 7 -> нет
- 
-// Console.WriteLine("Введите число");
-// int a = Convert.ToInt32(Console.ReadLine());
-// if (a % 2 == 0)
-// System.Console.WriteLine("Число: " + a + " четное");
-// else System.Console.WriteLine($"Число: {a} нечетное");
+// int num, r, sum=0,t;
+// Console.Write("Введите пятизначное число: ");
+// num = Convert.ToInt32(Console.ReadLine());
+// if(num>9999 && num<100000)
+// {
+// for(t=num;num!=0;num=num/10)
+// {
+// r=num % 10;
+// sum=sum*10+r;
+// }
+// if(t==sum)
+// Console.Write($"{t} является палиндромом.");
+// else
+// Console.Write($"{t} не является палиндромом.");
+// } else
+// Console.Write("Введите пятизначное число! ");
 
-// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-// 5 -> 2, 4
-// 8 -> 2, 4, 6, 8
- 
-Console.WriteLine("Введите число N");
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// int InputN(string message)
+// {
+// System.Console.Write($"{message}> ");
+// int value;
+// if (int.TryParse(Console.ReadLine(), out value))
+// {
+// return value;
+// }
+// System.Console.WriteLine("Вы ввели не число");
+// Environment.Exit(1);
+// return 0;
+
+// }
+
+// int x1 = InputN ("Введите координату X точки A");
+// int y1 = InputN ("Введите координату Y точки A");
+// int z1 = InputN ("ведите координату Z точки A");
+// int x2 = InputN ("Введите координату X точки B");
+// int y2 = InputN ("Введите координату Y точки B");
+// int z2 = InputN ("Введите координату Z точки B");
+
+// double dis(int x1, int y1, int z1, int x2, int y2, int z2)
+// {
+// int deltax = x2 - x1;
+// int deltay = y2 - y1;
+// int deltaz = z2 - z1;
+// return Math.Sqrt(deltax*deltax+deltay*deltay+deltaz*deltaz);
+// }
+// System.Console.Write($"Растояние между точками A и B равно {dis(x1, y1, z1, x2, y2, z2):F2} ");
+
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число N: ");
 int n = Convert.ToInt32(Console.ReadLine());
-for(int i=1;i<=n;i++)
+Console.Write("Таблица кубов:\n");
+for (int i=1;i<=n;i++)
 {
-if (i % 2 == 0)
-System.Console.Write(i + ", ");
+int k = i*i*i;
+Console.Write($"{k}, ");
 }
